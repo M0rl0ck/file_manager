56bbbdf7cb305d9ed64e9559e3fefb6e7897ee53
M0rl0ck <seriy17@yandex.ru>
2024-02-01 23:28:06 +0300

    feat: add cat commamd

--- src/tools/fs/fs.js
+++ src/tools/fs/fs.js
@@ -1,19 +1,36 @@
 import path from "node:path";
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+import { ERROR } from "../../constants/errors.js";
+import { pipeline, finished } from "node:stream/promises";
+import { Writable } from "node:stream";
+
+const customStdOut = () =>
+  new Writable({
+    decodeStrings: false,
+    write(chunk, encoding, callback) {
+      stdout.write(chunk);
+      callback();
+    },
+  });
 
 class FS {
   constructor(cwd) {
     this.cwd = cwd;
   }
-  cat = async (pathToFile) => {
-    const patchToReadingFile = path.resolve(this.cwd.cur);
+  cat = async (pathToFile = "") => {
+    const patchToReadingFile = path.resolve(
+      this.cwd.currentDirectory,
+      pathToFile
+    );
+    console.log(patchToReadingFile);
     try {
-      const fileText = await readFile(patchToReadingFile, "utf-8");
-      console.log(fileText);
-    } catch (e) {
-      if (e.code === "ENOENT") {
-        throw new Error(ERRORMESSAGE);
-      }
-      console.log(e.message);
+      const input = createReadStream(patchToReadingFile, { encoding: "utf-8" });
+      stdout.write("\n");
+      await pipeline(input, customStdOut());
+      stdout.write("\n");
+    } catch {
+      throw new Error(ERROR.OPERATION_FAILED);
     }
   };
 }
